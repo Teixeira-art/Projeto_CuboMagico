4b9b9f540401f890753cdd84a2e138c1323db499
Lucas Teixeira Holanda <69094870+Teixeira-art@users.noreply.github.com>
2026-03-15 01:12:29 -0300

    Update recognize_color.py

--- recognize_color.py
+++ recognize_color.py
@@ -5,8 +5,10 @@ cam = cv2.VideoCapture(0)
 corChange = ''
 corFinal = ''
 
-cube_side = np.empty((3, 3), dtype=object)
-cubo = np.empty((6,3,3),dtype=object)
+#os lados do cubo e o cubo total são strings vazias, já pensando na implementação com o kocyemba
+cubo = ""
+
+side = 0
 
 while True:
     _, img = cam.read()
@@ -14,7 +16,6 @@ while True:
     offset = 100
 
     lado = min(w, h) - 2 * offset
-
 
     x1 = (w - lado) // 2
     y1 = (h - lado) // 2
@@ -24,49 +25,56 @@ while True:
     campo = img[y1:y2, x1:x2]
     cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), 3)
 
-    #DESENHA OS 9 MINI-QUADRADOS (3x3) DENTRO DO QUADRADO GRANDE
-    cell = lado // 3
-    
-    for row in range(3):
-        for col in range(3):
-            xx1 = x1 + col * cell
-            yy1 = y1 + row * cell
+    cube_side = ""
 
-            # fecha certinho no limite do quadrado grande na última linha/coluna
-            xx2 = x1 + (col + 1) * cell if col < 2 else x2
-            yy2 = y1 + (row + 1) * cell if row < 2 else y2
-          
+    # 9 quadrados
+    cell = lado // 3
+
+    for i in range(3):
+        for j in range(3):
+            xx1 = x1 + j * cell
+            yy1 = y1 + i * cell
+
+            xx2 = x1 + (j + 1) * cell if j < 2 else x2
+            yy2 = y1 + (i + 1) * cell if i < 2 else y2
+
             cv2.rectangle(img, (xx1, yy1), (xx2, yy2), (255, 0, 0), 1)
-          
+
             piece = img[yy1:yy2 , xx1:xx2]
             media = np.average(piece, axis =(0,1))
-          
+
             r, g, b = int(media[2]), int(media[1]), int(media[0])
             cor = [r, g, b]
-        
 
-            if r >= 100 and g >= 100 and b >= 110:
-                corFinal = 'Branco'
-            elif r >= 100 and g >= 100 and b <= 80
-                corFinal = 'Amarelo'
+            if r >= 100 and g >= 100 and b >= 110: 
+                corFinal = 'U' #Branco
+            elif r >= 100 and g >= 100 and b <= 80:
+                corFinal = 'B' #Amarelo
             elif r >= 140 and g >= 65 and g < 140 and b <= 80:
-                corFinal = 'Laranja'
+                corFinal = 'L' #Laranja
             elif np.argmax(cor) == 0:
-                corFinal = 'Vermelho'
+                corFinal = 'R' #Vermelho
             elif np.argmax(cor) == 1:
-                 corFinal = 'Verde'
+                corFinal = 'F' #Verde
             elif np.argmax(cor) == 2:
-                  corFinal = 'Azul'
+                corFinal = 'B' #Azul
 
-            cube_side[row][col] = corFinal
+            cube_side = cube_side + corFinal
 
     key = cv2.waitKey(10) & 0xFF
 
-    if key == ord('m'):
+    if key == ord('s'):
+        cubo = cubo + cube_side
+        print("Face salva:", side)
         print(cube_side)
+        side += 1
 
-
-
+    if side == 6:
+        print("Cubo completo!")
+        break
 
     cv2.imshow('Img', img)
-    cv2.waitKey(10)
+
+
+print("Scramble:" + cubo)
+
